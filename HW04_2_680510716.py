82e3e481dac394fe1ede924f46f7049229cf8583
FjordPolaris <yorushika.lyrah@gmail.com>
2025-07-19 00:12:40 +0700

    Redo

--- HW04_2_680510716.py
+++ HW04_2_680510716.py
@@ -17,11 +17,11 @@ def test_calculate_exp() -> None:
 
 
 # FUNCTIONS
-def evolve(amount: int, candy: int, required_candy: int) -> tuple[int, int, int] | None:
-    if candy < required_candy: return
-    
-    evolution_counts: int = min(amount, candy // (required_candy - amount))
-    total_candy_required: int = required_candy * evolution_counts
+def evolve(amount: int, candy: int, candy_per_evolution: int) -> tuple[int, int, int] | None:
+    if candy < candy_per_evolution: return
+
+    evolution_counts: int = min(amount, candy // (candy_per_evolution-amount))
+    total_candy_required: int = candy_per_evolution * evolution_counts
 
     reward_candy: int = evolution_counts
     reward_exp: int = evolution_counts * 1000
@@ -29,21 +29,39 @@ def evolve(amount: int, candy: int, required_candy: int) -> tuple[int, int, int]
     return reward_exp, reward_candy, total_candy_required
 
 
+def calculate_reward(amount: int, candy: int) -> tuple[int, int]:
+    EVOLUTION_COST: int = 12
+
+    current_candy: int = candy
+    current_exp: int = 0
+
+    reward_exp, reward_candy, required_candy = evolve(amount, current_candy, EVOLUTION_COST) or (0, 0, 0)
+    current_exp += reward_exp
+    current_candy -= required_candy
+    current_candy += reward_candy
+
+    return current_exp, current_candy
+
+
 def calculate_exp(p: int, c: int) -> int:
     if (p <= 0) or (c < 12): return 0 
-
-    REQUIRED_CANDY: int = 12
     
-    current_exp: int = 0
+    remaining_amount: int = 0
+    if p: remaining_amount = p
+
     current_candy: int = 0
-
+    current_exp: int = 0
     if c: current_candy = c
+    
+    current_exp, current_candy = calculate_reward(p, c)
 
-    reward_exp, reward_candy, required_candy = evolve(p, current_candy, REQUIRED_CANDY) or (0, 0, 0)
-    if reward_exp > 0:
-        current_exp += reward_exp
-        current_candy -= required_candy
-        current_candy += reward_candy
+    if (remaining_amount > 0) and (current_candy == 11):
+        remaining_amount -= 1
+        current_candy += 1
+
+        retured_exp, retured_candy = calculate_reward(remaining_amount, current_candy)
+        current_candy += retured_candy
+        current_exp += retured_exp
 
     return current_exp
 
