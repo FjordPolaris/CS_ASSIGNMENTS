7bfaf571ca2c6cfd275949e78cf34f34d3914065
FjordPolaris <yorushika.lyrah@gmail.com>
2025-07-19 14:06:53 +0700

    Fixed

--- HW04_2_680510716.py
+++ HW04_2_680510716.py
@@ -4,13 +4,15 @@
 # HW04_2
 # 204111 Sec 003
 
+DEBUG: bool = False
+
+
 def test_calculate_exp() -> None:
     assert calculate_exp(1, 12) == 1000
     assert calculate_exp(2, 12) == 1000
     assert calculate_exp(2, 22) == 2000
     assert calculate_exp(1, 55) == 1000
     assert calculate_exp(3, 33) == 3000
-    assert calculate_exp(10, 100) == 10000
     assert calculate_exp(1, 0) == 0
     assert calculate_exp(-1, -1) == 0
     print(":>")
@@ -18,53 +20,33 @@ def test_calculate_exp() -> None:
 
 # FUNCTIONS
 
-def evolve(amount: int, candy: int, candy_per_evolution: int) -> tuple[int, int, int] | None:
-    if candy < candy_per_evolution: return
+def evolve(amount: int, candy: int) -> int | None:
+    DEFAULT_EVOLUTION_COST: int = 12
+    DEFAULT_REWARD_EXP: int = 1000
+    
+    pidgey: int = amount or 0
+    max_candy: int = (candy + pidgey) // (DEFAULT_EVOLUTION_COST-1)
+    evolutions: int = min(pidgey, max_candy) 
 
-    evolution_counts: int = min(amount, candy // (candy_per_evolution-amount))
-    total_candy_required: int = candy_per_evolution * evolution_counts
+    reward_exp: int = DEFAULT_REWARD_EXP * evolutions
 
-    reward_candy: int = evolution_counts
-    reward_exp: int = evolution_counts * 1000
-
-    return reward_exp, reward_candy, total_candy_required
-
-
-def calculate_reward(amount: int, candy: int) -> tuple[int, int]:
-    EVOLUTION_COST: int = 12
-
-    current_candy: int = candy
-    current_exp: int = 0
-
-    reward_exp, reward_candy, required_candy = evolve(amount, current_candy, EVOLUTION_COST) or (0, 0, 0)
-    current_exp += reward_exp
-    current_candy -= required_candy
-    current_candy += reward_candy
-
-    return current_exp, current_candy
+    return reward_exp
 
 
 def calculate_exp(p: int, c: int) -> int:
-    if (p <= 0) or (c < 12): return 0 
-    
-    remaining_amount: int = 0
-    if p: remaining_amount = p
+    if (p < 1) or (p + c < 13): return 0
 
-    current_candy: int = 0
+    amount: int = p or 0
+
+    current_candy: int = c or 0
     current_exp: int = 0
-    if c: current_candy = c
-   
-    # INITIAL CALCULATION
-    current_exp, current_candy = calculate_reward(p, c)
 
-    # WHEN PIDGEY NEED TO BE TURNED INTO CANDY
-    if (remaining_amount > 0) and (current_candy == 11):
-        remaining_amount -= 1
-        current_candy += 1
+    reward_exp = evolve(amount, current_candy) or 0
+    current_exp += reward_exp
 
-        retured_exp, retured_candy = calculate_reward(remaining_amount, current_candy)
-        current_candy += retured_candy
-        current_exp += retured_exp
+    if DEBUG:
+        print("EXP:", current_exp)
+        print(" ")
 
     return current_exp
 
