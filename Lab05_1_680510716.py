28b3f356e2951e31e6281ddf1551397d94afa566
FjordPolaris <yorushika.lyrah@gmail.com>
2025-07-24 19:06:40 +0700

    Fixed

--- Lab05_1_680510716.py
+++ Lab05_1_680510716.py
@@ -11,42 +11,33 @@ def test_is_valid_license() -> None:
     assert is_valid_license("CD700") == True
     assert is_valid_license("99D1234") == False
     assert is_valid_license("9ab8954") == False
-    assert is_valid_license("") == False
-    assert is_valid_license("99999999") == False
-    assert is_valid_license("9aaaaa") == False
-    assert is_valid_license("1AA") == True 
+    assert is_valid_license("2Ab343") == False
     assert is_valid_license("a") == False
+    assert is_valid_license("") == False
     print(":>")
 
 
 def is_valid_license(license_str: str) -> bool:
-    # Preliminary validations
-    if (len(license_str) > 7 or 
-        len(license_str) < 1 or 
-        str.isdigit(license_str) or 
-        str.islower(license_str)): 
-        return False
-    
-    if str.isdigit(license_str[1]) == False and len(license_str) > 1:
-        # Alphabet validations
-        if str.isdigit(license_str[0]):
-            if not str.isdigit(license_str[1:3]) and not len(license_str) > 3:
-                return True
-            elif not str.isdigit(license_str[1:3]) and len(license_str) > 3 and str.isdigit(license_str[3:]):
-                return True
-        else:
-            if not str.isdigit(license_str[0:2]) and not len(license_str) > 2:
-                return True
-            if len(license_str) > 2 and str.isdigit(license_str[2:]):
-                return True
+    _license: str = license_str.strip()
 
-    elif str.isdigit(license_str[0:]):
-        return True
+    # Preliminary validations
+    if (len(_license) > 7 or 
+        len(_license) < 1 or 
+        str.isdigit(_license) or 
+        not str.isupper(_license)): 
+        return False
+
+    if str.isdigit(_license[0]) and str.isalpha(_license[1:3]) and str.isupper(_license):
+        if len(_license) > 3 and str.isdigit(_license[3:]):
+                return True
+    elif str.isalpha(_license[0:2]) and str.isupper(_license):
+        if str.isdigit(_license[2:]):
+            return True
     else:
         return False
 
     return False
-
+    
 
 if __name__ == "__main__":
     test_is_valid_license()
