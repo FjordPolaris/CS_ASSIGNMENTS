3ebfb6cd9f1e40f0a77cdd35d3c2a13df89d8908
FjordPolaris <yorushika.lyrah@gmail.com>
2025-08-01 20:49:59 +0700

    Finalised

--- WEEK_06/Lab06_2_680510716.py
+++ WEEK_06/Lab06_2_680510716.py
@@ -5,27 +5,19 @@
 # 204111 Sec 003
 
 
-def test_dest_rotate_list():
-    list_a: list[int] = [1, 2, 3, 4]
-    
-    dest_rotate_list(list_a, 1)
-    dest_rotate_list(list_a, 2)
-    dest_rotate_list(list_a, 105)
-    dest_rotate_list(list_a, -1)
-    dest_rotate_list(list_a, -2)
-
-
 def dest_rotate_list(list_a: list[int], n: int):
-    if not n:
-        print("output =", list_a),
-    
     times: int = n % len(list_a)
     list_a[:] = list_a[-times:] + list_a[:-times]
     
-    print("output =", list_a)
     del times
     #return list_alpha
 
 
 if __name__ == "__main__":
-    test_dest_rotate_list()
+    list_a = [1, 2, 3, 4]
+    dest_rotate_list(list_a, 1)
+    print('output =', list_a)
+
+    list_a = [1, 2, 3, 4]
+    dest_rotate_list(list_a, 105)
+    print('output =', list_a)
